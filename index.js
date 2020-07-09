4e3e9950479199ad4d033b29779a24597f9448ba
Joseph Rehfuss <josephrehfuss@fvfxvbl0hv22.domain>
2020-07-09 10:19:12 -0700

    added turtle route

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get('/',(req,res)=>{
     res.send('I like turtles!');
 })
 
+app.get("/turtle",(req,res)=>{
+    res.json({
+        name:"leo",
+        isTurtle:true,
+        isTeenageMutant:true
+    })
+})
+
 app.listen(PORT,function(){
     console.log("listenin on port "+ PORT);
 })
